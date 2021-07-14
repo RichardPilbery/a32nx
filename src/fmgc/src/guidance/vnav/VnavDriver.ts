210c2d2153feaf9b6747558a0a607d7d1ad80502
Will Pine <wpine215@gmail.com>
2021-07-14 13:57:49 -0700

    Gear and flap configurations now affect drag coefficient

--- src/fmgc/src/guidance/vnav/VnavDriver.ts
+++ src/fmgc/src/guidance/vnav/VnavDriver.ts
@@ -509,21 +509,42 @@ class FlightModel {
 
     /**
      * Get drag coefficient at given conditions
-     * TODO: support more mach numbers & flap and gear drag increments
      * @param weight in pounds
      * @param mach self-explanatory
      * @param delta pressure at the altitude divided by the pressure at sea level
-     * @param spdBrkDeflected Whether speedbrake is deflected at half or not
+     * @param spdBrkDeflected whether speedbrake is deflected at half or not
+     * @param gearExtended whether gear is extended or not
+     * @param flapConf flap configuration
      * @returns drag coefficient (Cd)
      */
-    static getDragCoefficient(weight: number, mach: number, delta: number, spdBrkDeflected: boolean) : number {
+    static getDragCoefficient(weight: number, mach: number, delta: number, spdBrkDeflected = false, gearExtended = false, flapConf = FlapConf.CLEAN) : number {
         const Cl = this.getLiftCoefficient(weight, mach, delta);
 
-        // For mach 0.78 only
-        const cleanConfigDrag = (0.0384 * Cl ** 5) - (0.1385 * Cl ** 4) + (0.1953 * Cl ** 3) - (0.0532 * Cl ** 2) - (0.0052 * Cl) + 0.0259;
+        // Values taken at mach 0.78
+        let baseDrag;
+        switch (flapConf) {
+        case FlapConf.CLEAN:
+            baseDrag = (0.0384 * Cl ** 5) - (0.1385 * Cl ** 4) + (0.1953 * Cl ** 3) - (0.0532 * Cl ** 2) - (0.0052 * Cl) + 0.0259;
+            break;
+        case FlapConf.CONF_1:
+            baseDrag = (0.0438 * Cl ** 5) - (0.1911 * Cl ** 4) + (0.3215 * Cl ** 3) - (0.1801 * Cl ** 2) + (0.0281 * Cl) + 0.0441;
+            break;
+        case FlapConf.CONF_2:
+            baseDrag = (0.0116 * Cl ** 5) - (0.0593 * Cl ** 4) + (0.1292 * Cl ** 3) - (0.0858 * Cl ** 2) + (0.0043 * Cl) + 0.0895;
+            break;
+        case FlapConf.CONF_3:
+            baseDrag = (0.01 * Cl ** 5) - (0.0558 * Cl ** 4) + (0.1325 * Cl ** 3) - (0.1019 * Cl ** 2) + (0.0123 * Cl) + 0.1004;
+            break;
+        case FlapConf.CONF_FULL:
+            baseDrag = (0.0014 * Cl ** 5) - (0.0097 * Cl ** 4) + (0.0369 * Cl ** 3) - (0.0222 * Cl ** 2) - (0.0201 * Cl) + 0.1534;
+            break;
+        default:
+            break;
+        }
 
         const spdBrkIncrement = spdBrkDeflected ? 0.01 : 0;
-        return cleanConfigDrag + spdBrkIncrement;
+        const gearIncrement = gearExtended ? 0.03 : 0;
+        return baseDrag + spdBrkIncrement + gearIncrement;
     }
 
     /**
@@ -532,10 +553,12 @@ class FlightModel {
      * @param mach self-explanatory
      * @param delta pressure at the altitude divided by the pressure at sea level
      * @param spdBrkDeflected Whether speedbrake is deflected at half or not
+     * @param gearExtended whether gear is extended or not
+     * @param flapConf flap configuration
      * @returns drag
      */
-    static getDrag(weight: number, mach: number, delta: number, spdBrkDeflected: boolean): number {
-        const Cd = this.getDragCoefficient(weight, mach, delta, spdBrkDeflected);
+    static getDrag(weight: number, mach: number, delta: number, spdBrkDeflected: boolean, gearExtended: boolean, flapConf: FlapConf): number {
+        const Cd = this.getDragCoefficient(weight, mach, delta, spdBrkDeflected, gearExtended, flapConf);
         return 1481.4 * (mach ** 2) * delta * this.wingArea * Cd;
     }
 
@@ -609,6 +632,14 @@ enum VnavState {
     REPRESSURIZATION,
     IDLE,
     GEOMETRIC
+}
+
+enum FlapConf {
+    CLEAN,
+    CONF_1,
+    CONF_2,
+    CONF_3,
+    CONF_FULL
 }
 
 interface Constraint {
